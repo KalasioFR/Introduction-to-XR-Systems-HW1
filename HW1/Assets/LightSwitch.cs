8ffdcea3ec9aabdaa555cf05aad3fdc6a316a819
KalasioFR <kalasio.games1@gmail.com>
2024-01-26 21:28:11 +0200

    Fixed the light switch

--- HW1/Assets/LightSwitch.cs
+++ HW1/Assets/LightSwitch.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LightSwitch : MonoBehaviour
 {
+    public InputActionReference action;
     public new Light light;
+    private bool isYellow = false;
+
     void Start()
     {
         light = GetComponent<Light>();
-    }
-    void Update()
-    {
-        if (Input.GetKeyDown("tab"))
+        action.action.Enable();
+        action.action.performed += (ctx) =>
         {
-            light.color = Color.yellow;
-        }
+            if (isYellow)
+            {
+                light.color = Color.white;
+            }
+            else
+            {
+                light.color = Color.yellow;
+            }
+
+            isYellow = !isYellow;
+        };
     }
 }
